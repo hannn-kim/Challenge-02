0e879b243f3e90cde9f0fc7406edd1c47126d927
hannn-kim <rlagkdms030918@gmail.com>
2026-04-15 13:13:51 +0900

    Initial Commit

---
+++ Challenge02/Challenge02App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge02App.swift
+//  Challenge02
+//
+//  Created by Han on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
